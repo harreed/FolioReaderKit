9447e837770fb9dba35fe473f502257e0f904093
Harry Davies <hjdavies@live.co.uk>
2022-09-26 21:22:28 +0100

    Why why why

--- Package.swift
+++ Package.swift
@@ -4,8 +4,16 @@ import PackageDescription
 
 let package = Package(
     name: "FolioReaderKit",
-    platforms: [.iOS(.v13)],
-	dependencies: [
+    platforms: [
+        .iOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "FolioReaderKit",
+            type: .static,
+            targets: ["FolioReader"]),
+    ],
+    dependencies: [
         .package(url: "https://github.com/ZipArchive/ZipArchive.git", from: Version(2, 1, 0)),
         .package(url: "https://github.com/cxa/MenuItemKit.git", from: Version(3, 0, 0)),
         .package(url: "https://github.com/zoonooz/ZFDragableModalTransition.git", from: Version(0, 6, 0)),
@@ -13,5 +21,13 @@ let package = Package(
         .package(url: "https://github.com/ArtSabintsev/FontBlaster.git", from: Version(4, 0, 0)),
         .package(url: "https://github.com/fantim/JSQWebViewController.git", from: Version(6, 1, 0)),
         .package(url: "https://github.com/realm/realm-cocoa.git", from: Version(3, 1, 0)),
-	]
+    ],
+    targets: [
+        .target(
+            name: "FolioReader",
+            dependencies: [],
+            path: "./FolioReader",
+            sources: ["Source"]
+        ),
+    ]
 )
