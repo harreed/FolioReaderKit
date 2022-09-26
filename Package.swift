d61fb55d317870af11806a441e76e88f8a1d6563
Harry Davies <hjdavies@live.co.uk>
2022-09-26 21:25:14 +0100

    dont disobey me

--- Package.swift
+++ Package.swift
@@ -1,18 +1,19 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "FolioReaderKit",
     platforms: [
-        .iOS(.v13),
-    ],
+            .iOS(.v13)
+        ],
     products: [
         .library(
-            name: "FolioReaderKit",
-            type: .static,
+            name: "FolioReader",
             targets: ["FolioReader"]),
     ],
+    
     dependencies: [
         .package(url: "https://github.com/ZipArchive/ZipArchive.git", from: Version(2, 1, 0)),
         .package(url: "https://github.com/cxa/MenuItemKit.git", from: Version(3, 0, 0)),
@@ -20,14 +21,11 @@ let package = Package(
         .package(url: "https://github.com/tadija/AEXML.git", from: Version(4, 2, 0)),
         .package(url: "https://github.com/ArtSabintsev/FontBlaster.git", from: Version(4, 0, 0)),
         .package(url: "https://github.com/fantim/JSQWebViewController.git", from: Version(6, 1, 0)),
-        .package(url: "https://github.com/realm/realm-cocoa.git", from: Version(3, 1, 0)),
+        .package(url: "https://github.com/realm/realm-cocoa.git", from: Version(3, 1, 0))
     ],
     targets: [
         .target(
             name: "FolioReader",
-            dependencies: [],
-            path: "./FolioReader",
-            sources: ["Source"]
-        ),
+            dependencies: []),
     ]
 )
