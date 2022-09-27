0ffd8d0d414421eefff8e8e4bb7c79fadb9963f1
Harry Davies <hjdavies@live.co.uk>
2022-09-27 20:36:31 +0100

    Test upgrade AEXML

--- Package.swift
+++ Package.swift
@@ -13,14 +13,13 @@ let package = Package(
             name: "FolioReaderKit",
             targets: ["FolioReaderKit"]),
     ],
-    
     dependencies: [
         .package(url: "https://github.com/ZipArchive/ZipArchive.git", from: Version(2, 1, 0)),
         .package(url: "https://github.com/cxa/MenuItemKit.git", from: Version(3, 0, 0)),
         .package(url: "https://github.com/zoonooz/ZFDragableModalTransition.git", from: Version(0, 6, 0)),
-        .package(url: "https://github.com/tadija/AEXML.git", from: Version(4, 2, 0)),
         .package(url: "https://github.com/ArtSabintsev/FontBlaster.git", from: Version(4, 0, 0)),
         .package(url: "https://github.com/fantim/JSQWebViewController.git", from: Version(6, 1, 0)),
+        .package(url: "https://github.com/tadija/AEXML.git", .upToNextMinor(from: "4.6.1")),
         .package(url: "https://github.com/realm/realm-cocoa.git", from: Version(3, 1, 0))
     ],
     targets: [
