e01b1ca014e62637c579e85f2cc21a6a4bc41686
Harry Davies <hjdavies@live.co.uk>
2022-09-27 21:32:19 +0100

    Some fun

--- Package.swift
+++ Package.swift
@@ -16,16 +16,19 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/ZipArchive/ZipArchive.git", from: Version(2, 1, 0)),
         .package(url: "https://github.com/cxa/MenuItemKit.git", from: Version(3, 0, 0)),
-        .package(url: "https://github.com/zoonooz/ZFDragableModalTransition.git", from: Version(0, 6, 0)),
         .package(url: "https://github.com/ArtSabintsev/FontBlaster.git", from: Version(4, 0, 0)),
-        .package(url: "https://github.com/fantim/JSQWebViewController.git", from: Version(6, 1, 0)),
         .package(url: "https://github.com/tadija/AEXML.git", .upToNextMinor(from: "4.6.1")),
         .package(url: "https://github.com/realm/realm-cocoa.git", from: Version(3, 1, 0))
     ],
     targets: [
         .target(
             name: "FolioReaderKit",
-            dependencies: [],
+            dependencies: ["AEXML",
+                           "FontBlaster",
+                           "Realm",
+                           "RealmDatabase",
+                           "ZipArchive",
+                           "MenuItemKit"],
             path: "Source"),
     ]
 )
